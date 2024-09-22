02fa870c7939066779a20b5c3b5139c7ff95c9b2
Ayyappa Reddy Nandipati <119839645+ayyappareddynandipati@users.noreply.github.com>
2024-09-22 09:07:46 +0530

    Time: 1899 ms (34.51%), Space: 7.2 MB (84.44%) - LeetHub

---
+++ 0507-perfect-number/0507-perfect-number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool checkPerfectNumber(int num) {
+        int pos_div_sum=0;
+        for(int i=1;i<num;i++)
+        {
+            if(num%i==0)
+            {
+                pos_div_sum+=i;
+            }
+            else if (pos_div_sum>num)
+            {
+                return false;
+            }
+        }
+        return pos_div_sum==num;
+    }
+};
