0bd3e3b99d64f49b0346b78e35e9294c6aeae4b3
Ayyappa Reddy Nandipati <119839645+ayyappareddynandipati@users.noreply.github.com>
2024-11-26 07:23:59 +0530

    Time: 0 ms (100%), Space: 8.8 MB (31.17%) - LeetHub

---
+++ 3312-number-of-changing-keys/3312-number-of-changing-keys.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int countKeyChanges(string s) {
+        int cnt=0;
+        for(int i=1;i<s.size();i++)
+        {
+            if(tolower(s[i-1])!=tolower(s[i]))
+                {
+                    cnt++;
+                }
+        }
+        return cnt;
+    }
+};
