101613559801c43801b29acbc975b8257ba843be
Ayyappa Reddy Nandipati <119839645+ayyappareddynandipati@users.noreply.github.com>
2024-12-17 18:56:30 -0800

    Time: 0 ms (100%), Space: 13.9 MB (29.91%) - LeetHub

---
+++ 1570-final-prices-with-a-special-discount-in-a-shop/1570-final-prices-with-a-special-discount-in-a-shop.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> finalPrices(vector<int>& prices) {
+        int n = prices.size();
+        vector<int> res(n);
+        for(int i=0;i<n;i++)
+        {
+            res[i]=prices[i];
+            for(int j=i+1;j<n;j++)
+            {
+                if (prices[j] <= prices[i])
+                {
+                    res[i]=prices[i]-prices[j];
+                    break;
+                }
+            }
+        }
+        return res;
+    }
+};
