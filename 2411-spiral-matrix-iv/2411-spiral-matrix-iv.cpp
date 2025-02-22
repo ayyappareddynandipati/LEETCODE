f47c1b4eff0e614db1c9ee0d2f3e65e376569a3b
Nandipati Ayyappa Reddy <119839645+ayyappareddynandipati@users.noreply.github.com>
2025-02-22 08:41:38 +0530

    Time: 10 ms (99.49%), Space: 138.2 MB (28.73%) - LeetHub

--- 2411-spiral-matrix-iv/2411-spiral-matrix-iv.cpp
+++ 2411-spiral-matrix-iv/2411-spiral-matrix-iv.cpp
@@ -13,26 +13,26 @@ public:
     vector<vector<int>> spiralMatrix(int m, int n, ListNode* head) {
         vector<vector<int>> res(m,vector<int>(n,-1));
         int top=0,bottom=m-1,left=0,right=n-1;
-        while(top<=bottom && left<=right){
-            for(int i=left;i<=right && head!=nullptr;i++){
+        while(head){
+            for(int i=left;i<=right && head ;i++){
                 res[top][i]=head->val;
                 head=head->next;
             }
             top++;
-            for(int i=top;i<=bottom && head!=nullptr;i++){
+            for(int i=top;i<=bottom && head ;i++){
                 res[i][right]=head->val;
                 head=head->next;
             }
             right--;
             if(top<=bottom){
-                for(int i=right;i>=left && head!=nullptr;i--){
+                for(int i=right;i>=left && head ;i--){
                     res[bottom][i]=head->val;
                     head=head->next;
                 }
                 bottom--;
             }
             if(left<=right){
-                for(int i=bottom;i>=top && head!=nullptr;i--){
+                for(int i=bottom;i>=top && head ;i--){
                     res[i][left]=head->val;
                     head=head->next;
                 }
