a14fc261f7c0f57592c3cacd6fa2bd3b90a43807
Nandipati Ayyappa Reddy <119839645+ayyappareddynandipati@users.noreply.github.com>
2025-03-11 07:42:04 +0530

    Time: 14 ms (41.15%), Space: 11 MB (81.05%) - LeetHub

--- 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.cpp
@@ -4,12 +4,12 @@ public:
         int n = s.size();
         int left = 0;
         int cnt = 0;
-        unordered_map<char, int> freq;
-        for (int right = 0; right < n; ++right) {
-            freq[s[right]]++;
-            while (freq['a'] > 0 && freq['b'] > 0 && freq['c'] > 0) {
-                cnt += (n - right); 
-                freq[s[left]]--;
+        vector<int>count(3,0);
+        for (int i = 0; i < n; i++) {
+            count[s[i]-'a']++;
+            while (count[0] > 0 && count[1] > 0 && count[2] > 0) {
+                cnt += n-i; 
+                count[s[left]-'a']--;
                 left++;
             }
         }
