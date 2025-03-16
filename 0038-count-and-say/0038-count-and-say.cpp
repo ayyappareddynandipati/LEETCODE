85a651201ee1103343f0015eed37374d6ffd7749
Nandipati Ayyappa Reddy <119839645+ayyappareddynandipati@users.noreply.github.com>
2025-03-16 10:51:04 +0530

    Time: 3 ms (80.16%), Space: 9.9 MB (56.15%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.cpp
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -2,22 +2,17 @@ class Solution {
 public:
     string countAndSay(int n) {
         string val="1";
-        for(int i=0;i<n-1;i++){
-            char c = val[0];
+        for(int i=2;i<=n;i++){
             string s="";
             int cnt=1;
-            for(int j=1;j<val.length();j++){
-                if(c!=val[j]){
-                    s+=to_string(cnt);
-                    s+=c;
-                    cnt=1;
-                    c=val[j];
-                }else{
+            for(int j=0;j<val.length();j++){
+                if(j+1<val.length() && val[j]==val[j+1]){
                     cnt++;
+                }else{
+                    s+=to_string(cnt)+val[j];
+                    cnt=1;
                 }
             }
-            s+=to_string(cnt);
-            s+=c;
             val=s;
         }
         return val;
