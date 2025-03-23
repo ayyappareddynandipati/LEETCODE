2b48990e838a20203d12949850edc9b5d35b3ba7
Nandipati Ayyappa Reddy <119839645+ayyappareddynandipati@users.noreply.github.com>
2025-03-23 10:40:38 +0530

    Time: 0 ms (100%), Space: 12.5 MB (32.27%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -6,16 +6,14 @@ public:
         if(nums[0]>nums[1]) return 0;
         if(nums[n-1]>nums[n-2]) return n-1;
         int low=1,high=n-2;
-        while(low<=high){
+        while(low<high){
             int mid=(low+high)/2;
-            if(nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1]){
-                return mid;
-            }else if(nums[mid]>nums[mid-1]){
+            if(nums[mid]<nums[mid+1]){
                 low=mid+1;
             }else{
-                high=mid-1;
+                high=mid;
             }
         }
-        return -1;
+        return low;
     }
 };
