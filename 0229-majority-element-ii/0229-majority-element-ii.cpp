8af70e825f94d5da24fb217e8fc9f664216bfd61
Nandipati Ayyappa Reddy <119839645+ayyappareddynandipati@users.noreply.github.com>
2025-03-26 18:51:24 +0530

    Time: 0 ms (100%), Space: 20 MB (28.71%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -5,17 +5,18 @@ public:
         int n = nums.size();
         vector<int>res;
         for(int i=0;i<n;i++){
-            if(cnt1==0 && nums[i]!=ele2){
-                cnt1=1;
-                ele1=nums[i];
-            }else if(cnt2==0 && nums[i]!=ele1){
-                cnt2=1;
-                ele2=nums[i];
-            }else if (nums[i] == ele1){
+            if (nums[i] == ele1){
                 cnt1++;
             }
             else if (nums[i] == ele2){
                 cnt2++;
+            }
+            else if(cnt1==0){
+                cnt1=1;
+                ele1=nums[i];
+            }else if(cnt2==0){
+                cnt2=1;
+                ele2=nums[i];
             }else{
                 cnt1--,cnt2--;
             }
