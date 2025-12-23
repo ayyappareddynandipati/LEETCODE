725327f146229245d0f0e5c9d482e148c37186b9
Ayyappa Reddy Nandipati <119839645+ayyappareddynandipati@users.noreply.github.com>
2025-12-23 12:32:24 +0530

    Time: 0 ms (100%), Space: 43.4 MB (12.39%) - LeetHub

--- 2078-two-furthest-houses-with-different-colors/2078-two-furthest-houses-with-different-colors.java
+++ 2078-two-furthest-houses-with-different-colors/2078-two-furthest-houses-with-different-colors.java
@@ -3,13 +3,17 @@ class Solution {
         int n=colors.length;
         int max=0;
         for(int i=0;i<n-1;i++){
-            for(int j=i+1;j<n;j++){
-                if(colors[i]!=colors[j]){
-                    int diff=j-i;
-                    max=Math.max(max,diff);
+                if(colors[i]!=colors[n-1]){
+                    max=Math.max(max,n-1-i);
+                    break;
                 }
             }
-        }
+            for(int i=n-1;i>=1;i--){
+                  if(colors[0]!=colors[i]){
+                    max=Math.max(max,i);
+                    break;
+                }
+            }
         return max;
     }
 }
