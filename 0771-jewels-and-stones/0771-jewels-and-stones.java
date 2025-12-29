73a63d8f5af56188b36e4ec864f38e0e895e3542
Ayyappa Reddy Nandipati <119839645+ayyappareddynandipati@users.noreply.github.com>
2025-12-29 13:49:49 +0530

    Time: 1 ms (62.29%), Space: 43.4 MB (7.72%) - LeetHub

--- 0771-jewels-and-stones/0771-jewels-and-stones.java
+++ 0771-jewels-and-stones/0771-jewels-and-stones.java
@@ -1,21 +1,17 @@
 class Solution {
     public int numJewelsInStones(String jewels, String stones) {
-        int ans=0;
-       
-        for(int i=0;i<stones.length();i++){
-            char ch=stones.charAt(i);
-            boolean found=false;
-             for(int j=0;j<jewels.length();j++){
-                char ch1=jewels.charAt(j);
-                if(ch1==ch){
-                    found=true;
-                    break;
-                }
-             }
-             if(found==true){
-                ans=ans+1;
-             }
+       HashMap<Character,Integer> hm=new HashMap<>();
+       for(int i=0;i<stones.length();i++){
+        char ch=stones.charAt(i);
+        hm.put(ch,hm.getOrDefault(ch,0)+1);
+       }
+       int ans=0;
+       for(int i=0;i<jewels.length();i++){
+        char ch=jewels.charAt(i);
+        if(hm.containsKey(ch)){
+            ans += hm.get(ch);
         }
-        return ans;
+       }
+       return ans;
     }
 }
