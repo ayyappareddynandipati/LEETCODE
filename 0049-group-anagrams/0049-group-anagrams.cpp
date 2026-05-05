ccce803f0903e336e721e5503184c7bb5e02df13
Ayyappa Reddy Nandipati <119839645+ayyappareddynandipati@users.noreply.github.com>
2026-05-05 15:07:03 +0530

    Time: 10 ms (93.21%), Space: 26 MB (47.6%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string,vector<string>> anagrams;
+        for(string s : strs){
+            string key = s;
+            sort(key.begin(),key.end());
+            anagrams[key].push_back(s);
+        }
+        vector<vector<string>> result;
+        for(auto it : anagrams){
+            result.push_back(it.second);
+        }
+        return result;
+    }
+};
